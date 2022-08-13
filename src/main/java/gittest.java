fe68f3056c14df425e2fdb01c64d74fe4a92baa2
ZH <1062457423@qq.com>
2022-08-13 23:47:14 +0800

    test1

---
+++ src/main/java/gittest.java
@@ -0,0 +1,10 @@
+/**
+ * @author zhzsmbfa
+ * @create 2022-08-13  23:46
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("heheh");
+        System.out.println("asfas");
+    }
+}
